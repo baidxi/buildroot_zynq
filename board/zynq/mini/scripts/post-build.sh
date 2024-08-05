9282cf3d6adae880e717c2f06e13dd4e6ea7e40a
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2024-08-05 21:50:03 +0800

    up load config file
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

---
+++ board/zynq/mini/scripts/post-build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# genimage will need to find the extlinux.conf
+# in the binaries directory
+
+linux_image()
+{
+	if grep -Eq "^BR2_LINUX_KERNEL_UIMAGE=y$" ${BR2_CONFIG}; then
+		echo "uImage"
+	elif grep -Eq "^BR2_LINUX_KERNEL_IMAGE=y$" ${BR2_CONFIG}; then
+		echo "Image"
+	elif grep -Eq "^BR2_LINUX_KERNEL_IMAGEGZ=y$" ${BR2_CONFIG}; then
+		echo "Image.gz"
+	else
+		echo "zImage"
+	fi
+}
+
+PARTUUID="$($HOST_DIR/bin/uuidgen)"
+
+install -d "$TARGET_DIR/boot"
+
+sed -e "s/%LINUXIMAGE%/$(linux_image)/g" \
+	-e "s/%PARTUUID%/$PARTUUID/g" \
+	"board/zynq/mini/config/extlinux.conf" > "${BINARIES_DIR}/extlinux.conf"
+
+sed "s/%PARTUUID%/$PARTUUID/g" "board/zynq/mini/config/genimage.cfg" > "$BINARIES_DIR/genimage.cfg"
